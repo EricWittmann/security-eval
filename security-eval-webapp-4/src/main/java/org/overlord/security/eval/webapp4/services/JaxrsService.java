08cf5655490b85878218d3a2d1a856048627d9b1
Eric Wittmann <eric.wittmann@redhat.com>
2013-03-15 14:46:57 -0400

    Tweaked the saml creation.

--- security-eval-webapp-4/src/main/java/org/overlord/security/eval/webapp4/services/JaxrsService.java
+++ security-eval-webapp-4/src/main/java/org/overlord/security/eval/webapp4/services/JaxrsService.java
@@ -15,25 +15,18 @@
  */
 package org.overlord.security.eval.webapp4.services;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 import java.util.Set;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.xml.stream.FactoryConfigurationError;
-import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.HttpContext;
 import org.jboss.resteasy.client.ClientExecutor;
 import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
@@ -42,7 +35,6 @@ import org.jboss.security.SecurityContextAssociation;
 import org.picketlink.identity.federation.core.exceptions.ProcessingException;
 import org.picketlink.identity.federation.core.saml.v2.factories.SAMLAssertionFactory;
 import org.picketlink.identity.federation.core.saml.v2.util.AssertionUtil;
-import org.picketlink.identity.federation.core.saml.v2.writers.SAMLAssertionWriter;
 import org.picketlink.identity.federation.saml.v2.assertion.AssertionType;
 import org.picketlink.identity.federation.saml.v2.assertion.AttributeStatementType;
 import org.picketlink.identity.federation.saml.v2.assertion.AttributeStatementType.ASTChoiceType;
@@ -58,13 +50,10 @@ public class JaxrsService {
 
     private static final String ENDPOINT = "http://localhost:8080/security-eval-jaxrs/greeting";
 
-    private HttpServletRequest context;
-
     /**
      * Constructor.
      */
-    public JaxrsService(HttpServletRequest context) {
-        this.context = context;
+    public JaxrsService() {
     }
 
     /**
@@ -91,20 +80,14 @@ public class JaxrsService {
      */
     private ClientExecutor getSamlAssertionExecutor() {
         try {
-            final String b64Assertion = createB64Assertion(this.context.getUserPrincipal());
-
+            String username = "SAML-BEARER-TOKEN";
+            String password = createSAMLAssertion();
+            Credentials credentials = new UsernamePasswordCredentials(username, password);
             DefaultHttpClient httpClient = new DefaultHttpClient();
-            httpClient.addRequestInterceptor(new HttpRequestInterceptor() {
-                @Override
-                public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
-                    System.out.println("Setting HTTP Authorization to: " + b64Assertion);
-                    request.setHeader("Authorization", "Basic " + b64Assertion);
-                }
-            });
+            httpClient.getCredentialsProvider().setCredentials(AuthScope.ANY, credentials);
             ClientExecutor clientExecutor = new ApacheHttpClient4Executor(httpClient);
             return clientExecutor;
-        } catch (Throwable e) {
-            e.printStackTrace();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -118,8 +101,8 @@ public class JaxrsService {
      * @throws ProcessingException
      * @throws UnsupportedEncodingException
      */
-    protected static String createB64Assertion(final Principal principal) throws Exception,
-            FactoryConfigurationError, XMLStreamException, ProcessingException, UnsupportedEncodingException {
+    protected static String createSAMLAssertion() throws Exception {
+        Principal principal = SecurityContextAssociation.getPrincipal();
         NameIDType issuer = SAMLAssertionFactory.createNameID(null, null, "/security-eval-webapp-4");
         SubjectType subject = AssertionUtil.createAssertionSubject(principal.getName());
         AssertionType assertion = AssertionUtil.createAssertion(UUID.randomUUID().toString(), issuer);
@@ -129,17 +112,7 @@ public class JaxrsService {
         assertion.getConditions().addCondition(restriction);
         addRoleStatements(assertion, principal);
 
-        // Serialize the Assertion
-        XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        baos.write("SAML-BEARER-TOKEN:".getBytes("UTF-8"));
-        XMLStreamWriter xmlStreamWriter = xmlOutputFactory.createXMLStreamWriter(baos);
-        SAMLAssertionWriter writer = new SAMLAssertionWriter(xmlStreamWriter);
-        writer.write(assertion);
-        xmlStreamWriter.flush();
-        xmlStreamWriter.close();
-        String b64Assertion = new String(Base64.encodeBase64(baos.toByteArray()), "UTF-8");
-        return b64Assertion;
+        return AssertionUtil.asString(assertion);
     }
 
     /**
